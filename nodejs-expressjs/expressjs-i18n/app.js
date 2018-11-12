917572f74917ea63ddf0ad43ecf11a6c525ed7b2
Tri <tri.asmoro@online-pajak.com>
2018-11-12 11:53:29 +0700

    add i18n features on nodejs-expressjs-i18n

--- nodejs-expressjs/expressjs-i18n/app.js
+++ nodejs-expressjs/expressjs-i18n/app.js
@@ -3,11 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var i18n = require('i18n');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+i18n.configure({
+    locales:['en', 'id'],
+    directory: __dirname + '/locales'
+});
+
+// default: using 'accept-language' header to guess language settings
+app.use(i18n.init);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +30,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.get('/i18n', function(req, res){
+  var greeting = res.__('Hello');
+  res.send(greeting);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
